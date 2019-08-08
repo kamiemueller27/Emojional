c8b32ed372340f91bf4ce65d4b03c179e691772c
Apple <apple@Apples-MacBook-Pro-85.local>
2019-08-08 15:49:56 -0400

    iteration 1

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func treeButton(sender: UIButton) {
+        let alertController = UIAlertController(title: "SAVE TREES!!!", message: "We can save 75,000 trees if we recycled the paper used on the daily run of the New York Times alone!", preferredStyle: UIAlertController.Style.alert)
 
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated : true, completion : nil )
+    
+    }
+    @IBAction func oceanButton(sender: UIButton) {
+        let alertController = UIAlertController(title: "SAVE THE OCEAN!!!", message: "Each year, ocean pollution kills more than one million sea birds and discarded fishing nets kill approximately 300,000 dolphins and porpoises", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated : true, completion : nil )
+        
+    }
+    
+    
+    @IBAction func oceanButton(_ sender: Any) {
+    }
+    
+    @IBAction func treeButton(_ sender: Any) {
+    }
 
 }
+    
+
+
 
