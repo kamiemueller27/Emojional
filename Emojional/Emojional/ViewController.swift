b0c84f3e59e7f4eb19cf3d834c098d378453ff14
Apple <apple@Apples-MacBook-Pro-85.local>
2019-08-08 16:00:17 -0400

    iteraton 2 complete

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let emojis = ["🌳": "yay trees", "🌊": "yay ocean"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func treeButton(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
         let alertController = UIAlertController(title: "SAVE TREES!!!", message: "We can save 75,000 trees if we recycled the paper used on the daily run of the New York Times alone!", preferredStyle: UIAlertController.Style.alert)
 
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
@@ -23,6 +26,7 @@ class ViewController: UIViewController {
     
     }
     @IBAction func oceanButton(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
         let alertController = UIAlertController(title: "SAVE THE OCEAN!!!", message: "Each year, ocean pollution kills more than one million sea birds and discarded fishing nets kill approximately 300,000 dolphins and porpoises", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
